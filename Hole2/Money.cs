f3df6fe9967b43b5dfd02239a2526a84f2f0afcd
cvigueras <carlosj.vigueras@gmail.com>
2023-03-09 11:36:37 +0100

    Refactor: complete

---
+++ Hole2/Money.cs
@@ -0,0 +1,14 @@
+﻿namespace Hole2
+{
+    public class Money
+    {
+        public readonly int value;
+        public readonly string currency;
+
+        public Money(int value, string currency)
+        {
+            this.value = value;
+            this.currency = currency;
+        }
+    }
+}
---
+++ Hole3/Money.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Hole3
+{
+    public class Money
+    {
+        public readonly int value;
+        public readonly String currency;
+
+        public Money(int value, String currency)
+        {
+            this.value = value;
+            this.currency = currency;
+        }
+
+        public Money Plus(Money other)
+        {
+            if (!currency.Equals(other.currency))
+            {
+                throw new Incalculable();
+            }
+
+            return new Money(other.value + value, currency);
+        }
+    }
+}
